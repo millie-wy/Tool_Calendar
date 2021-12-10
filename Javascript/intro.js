3a507fbde171ee19f7beafa4a0c401491b00b1ad
Millie Cheung <cheungmillie9@gmail.com>
2021-12-10 09:47:21 +0100

    Display current date and time on sidebar

---
+++ Javascript/intro.js
@@ -0,0 +1,19 @@
+function initIntro() {
+    addEventListeners();
+    displayCurrentDateAndTime();
+}
+
+function addEventListeners() {
+    
+}
+
+/** Shows current date and time */
+function displayCurrentDateAndTime() {
+    const today = new Date();
+
+    const dateDisplay = document.querySelector('#current-date');
+    dateDisplay.innerHTML = today.toDateString();
+
+    const timeDisplay = document.querySelector('#current-time');
+    timeDisplay.innerHTML = today.getHours() + ":" + today.getMinutes();
+}
---
+++ Javascript/todo.js
@@ -0,0 +1,9 @@
+function initTodo() {
+    addEventListeners();
+
+}
+
+/** comment by Millie - this is created for you to put functions for buttons or keydown - refer to intro.js :) */
+function addEventListeners() {
+
+}
