db790085ff853b0a94fe98e663da84fd69fa31e4
Millie Cheung <cheungmillie9@gmail.com>
2021-12-10 17:06:25 +0100

    moved addEventListeners to main.js

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,12 +1,28 @@
-window.addEventListener('load', main, );
+window.addEventListener('load', main);
 
-// Functions to start on page load 
+/** Functions to start on page load */
 function main() {
-    addEventListeners();
     initIntro();
-    
+    initTodo();
     initCalendar();
+    addEventListeners();
 }
 
 // Gloabl variables 
 const today = new Date();
+
+
+function addEventListeners() {
+
+    /** Renders calender to previous month on button click */
+    document.querySelector('.prev').addEventListener('click', () => {
+    today.setMonth(today.getMonth() - 1);
+    renderCalendar();
+    })
+
+    /** Renders calender to next month on button click */
+    document.querySelector('.next').addEventListener('click', () => {
+    today.setMonth(today.getMonth() + 1);
+    renderCalendar();
+    })
+}
