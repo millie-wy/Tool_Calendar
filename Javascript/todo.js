695adeb04f21d4e02510a07f17b6064541a5a204
saralindqvist <sarathereselindqvist@hotmail.com>
2021-12-10 16:20:30 +0100

    finished the todo-list with js and html. small changes left.

--- Javascript/todo.js
+++ Javascript/todo.js
@@ -8,6 +8,7 @@ function addEventListeners() {
 const inputBox = document.querySelector(".inputField input");
 const addBtn = document.querySelector(".inputField button");
 const todoList = document.querySelector(".todoList");
+const deleteAllBtn = document.querySelector(".footer button");
 
 inputBox.onkeyup = ()=>{
     let userData = inputBox.value; // getting user entered value
@@ -30,18 +31,53 @@ addBtn.onclick = ()=> {
         todoArr.push(userData); // pushing or adding user data
         localStorage.setItem("New Todo", JSON.stringify(todoArr)); // transforming js object to a json string
         showTasks(); //calling showTasks function
+        addBtn.classList.remove("active") // unactivates the add button
 }
 
+//function to add task list inside ul
 function showTasks(){
     let getLocalStorage = localStorage.getItem("New Todo");
-    if(getLocalStorage == null) { // if localstorage is empty
+    if(getLocalStorage === null){ // if localstorage is empty
         todoArr = [] // blank array
-    } else{
+    }else{
         todoArr = JSON.parse(getLocalStorage); // transforming json string into a js object 
     }
+    const pendingNumber = document.querySelector(".pendingNumber");
+    pendingNumber.textContent = todoArr.length; //passing the lenght value in pendingNumber
+    if(todoArr.length > 0){ // if array length is greater than 0
+        deleteAllBtn.classList.add("active"); // active the clearall button
+    }else{
+        deleteAllBtn.classList.remove("active");// unactive the clearall button
+
+    }
+
     let newLiTag = '';
     todoArr.forEach((element, index) => {
-        newLiTag = `<li>${element}<span><i class="fas fa-trash"></i></span></li>`;
+        newLiTag += `<li>${element}<span onclick="deleteTask(${index})";><i class="fas fa-trash"></i></span></li>`;
     });
     todoList.innerHTML = newLiTag; // adding new li tag inside ul tag
+    inputBox.value = ""; // once task added leave the input 
 }
+
+// delete task function
+function deleteTask (index){
+    let getLocalStorage = localStorage.getItem("New Todo");
+    todoArr = JSON.parse(getLocalStorage); 
+    todoArr.splice(index, 1); //delete or remove the paricular indexed li
+    // after remove the li again update the local storage 
+    localStorage.setItem("New Todo", JSON.stringify(todoArr)); // transforming js object to a json string
+    showTasks(); //calling showTasks function
+}
+
+// delete all tasks function
+deleteAllBtn.onclick = ()=> {
+    todoArr = []; // empty an array
+    // after delete all task again update the local storage 
+    localStorage.setItem("New Todo", JSON.stringify(todoArr)); // transforming js object to a json string
+    showTasks(); //calling showTasks function
+}
+
+
+
+
+// LEFT TO DO: Pending task have to show 0. keep task when reload. Delete localstorage so it wont show old storage.
