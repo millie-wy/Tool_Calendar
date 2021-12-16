bf053bf3a2c9568bbae5d6cb33e5ed78746355a2
Millie Cheung <cheungmillie9@gmail.com>
2021-12-16 09:37:49 +0100

    Hide clear all button when today's todos is folded

--- Javascript/collapse-sidebar.js
+++ Javascript/collapse-sidebar.js
@@ -8,14 +8,17 @@ function toggleTodo(event) {
     let expandBtn = event.target;
     let parentDiv = event.target.parentNode;
     let todoList = parentDiv.nextElementSibling;
+    const clearBtn = document.getElementById('clear-all-btn');
         if (todoList.style.display !== "none") {
             todoList.style.display = "none";
             expandBtn.classList.remove('fa-minus-circle');
             expandBtn.classList.add('fa-plus-circle')
+            clearBtn.style.display = "none";
         } else {
           todoList.style.display = "block";
           expandBtn.classList.remove('fa-plus-circle');
-          expandBtn.classList.add('fa-minus-circle')
+          expandBtn.classList.add('fa-minus-circle');
+          clearBtn.style.display = "block";
         }
       }
 
